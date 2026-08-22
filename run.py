2cc629dfe7201b4f2104be6d9800bcac41f2c4d3
SpidLife <110126720+SpidLife@users.noreply.github.com>
2026-08-22 19:56:48 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
+import runpy
+import sys
+from pathlib import Path
 
+folder = Path(__file__).parent / "brow_lash_booking_bot_v4"
+sys.path.insert(0, str(folder))
+runpy.run_path(str(folder / "run.py"), run_name="__main__")
